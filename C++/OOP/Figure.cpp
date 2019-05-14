90247e21ac6fbe95917c246eddfabeba3a56fc4d
Bozhidar Dimov <bojko958@abv.bg>
2019-05-14 13:06:44 +0300

    added constructors and assignment operator

--- C++/OOP/Figure.cpp
+++ C++/OOP/Figure.cpp
@@ -6,6 +6,33 @@ Figure::Figure()
 {
 }
 
+Figure::Figure(const Figure & copy)
+{
+	//detect self-assignment
+	if (this == &copy)
+		return;
+
+	this->x = copy.x;
+	this->y = copy.y;
+	this->isActive = copy.isActive;
+}
+
+Figure::Figure(const Figure && other)
+{
+}
+
+Figure & Figure::operator=(const Figure & copy)
+{
+	//detect self-assignment
+	if (this == &copy)
+		return *this;
+
+	this->x = copy.x;
+	this->y = copy.y;
+	this->isActive = copy.isActive;
+	return *this;
+}
+
 void Figure::deactivate()
 {
 	this->isActive = false;
--- C++/OOP/Figure.h
+++ C++/OOP/Figure.h
@@ -9,13 +9,18 @@ protected:
 	int y;
 	bool isActive;
 public:
-	Figure();
+	/// The four BASE operations with objects
+	Figure();														//default constructor
+	Figure(const Figure &);							//copy-constructor
+	Figure(const Figure &&);						//move-constructor, its advanced mechanism in c++s
+	Figure& operator=(const Figure &);	//assignment operator
+	~Figure();													//destructor
 
 	void deactivate();
 
 	bool isAtPosition(int x, int y) const;
 	virtual bool moveTo(int x, int y, Figure * enemyFigures[FIGURES_PER_PLAYER]) = 0;
 
-	~Figure();
+	
 };
 
